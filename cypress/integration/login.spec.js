f9ace916dbc765171b651684079ae6b2f0e18376
psplery <roman.meister13@gmail.com>
2020-02-02 19:21:45 +0100

    Add  url assert and login button click

--- cypress/integration/login.spec.js
+++ cypress/integration/login.spec.js
@@ -1,7 +1,9 @@
 describe('My First Test', function() {
     it('Visits the Kitchen Sink', function() {
       cy.visit('/login')
+      cy.url().should('eq', 'https://app.moberries.com/login')
       cy.get('#email').type(Cypress.env('email'))
       cy.get('#password').type(Cypress.env('password'))
+      cy.get('form > .btn').click()
     })
   })
