956341277ac84585baefe01da83e21b943a03bd0
psplery <roman.meister13@gmail.com>
2020-02-02 17:32:51 +0100

    use cypress environment vars for login test

--- cypress/integration/login.spec.js
+++ cypress/integration/login.spec.js
@@ -1,7 +1,7 @@
 describe('My First Test', function() {
     it('Visits the Kitchen Sink', function() {
       cy.visit('/login')
-      cy.get('#email').type('TestUser')
-      cy.get('#password').type('TestPW')
+      cy.get('#email').type(Cypress.env('email'))
+      cy.get('#password').type(Cypress.env('password'))
     })
   })
