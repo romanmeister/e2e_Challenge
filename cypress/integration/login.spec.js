27dab87bf86d6e566d2029d4a6d721feea5dc121
psplery <roman.meister13@gmail.com>
2020-02-02 17:20:07 +0100

    Use baseUrl  for login test

--- cypress/integration/login.spec.js
+++ cypress/integration/login.spec.js
@@ -1,6 +1,6 @@
 describe('My First Test', function() {
     it('Visits the Kitchen Sink', function() {
-      cy.visit('https://app.moberries.com/login')
+      cy.visit('/login')
       cy.get('#email').type('TestUser')
       cy.get('#password').type('TestPW')
     })
