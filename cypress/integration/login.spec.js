51c9f62d97eab8ee2cf1c5fcee4dd89ff2e07aff
psplery <roman.meister13@gmail.com>
2020-02-02 16:48:30 +0100

    Add get login and pw field

--- cypress/integration/login.spec.js
+++ cypress/integration/login.spec.js
@@ -1,6 +1,7 @@
 describe('My First Test', function() {
     it('Visits the Kitchen Sink', function() {
       cy.visit('https://app.moberries.com/login')
-      
+      cy.get('#email').type('TestUser')
+      cy.get('#password').type('TestPW')
     })
   })
